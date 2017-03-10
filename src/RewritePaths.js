3d9a94953a821b155a21d502ebfce3a3041c683f
John Rivs <wyathfallensoul@gmail.com>
2017-03-10 15:45:04 +0100

    Refactor path rewriting

--- src/RewritePaths.js
+++ src/RewritePaths.js
@@ -1,33 +1,11 @@
 var config = require('./Config');
 
-config.sass.forEach((style, key) => {
-    config.sass[key].input = config.sass[key].input.map(filename => {
-        let prefix = config.sass[key].prefix || '';
+['sass', 'styles', 'js'].forEach(task => {
+    config[task].forEach((style, key) => {
+        config[task][key].input = config[task][key].input.map(filename => {
+            let prefix = config[task][key].prefix || '';
 
-        return `${config.paths.root}/${config.paths.sass}/${prefix}${filename}`;
-    });
-});
-
-config.js.forEach((style, key) => {
-    config.js[key].input = config.js[key].input.map(filename => {
-        let prefix = config.js[key].prefix || '';
-
-        return `${config.paths.root}/${config.paths.js}/${prefix}${filename}`;
-    });
-});
-
-config.styles.forEach((style, key) => {
-    config.styles[key].input = config.styles[key].input.map(filename => {
-        let prefix = config.styles[key].prefix || '';
-
-        return `${config.paths.root}/${config.paths.styles}/${prefix}${filename}`;
-    });
-});
-
-config.scripts.forEach((style, key) => {
-    config.scripts[key].input = config.scripts[key].input.map(filename => {
-        let prefix = config.scripts[key].prefix || '';
-
-        return `${config.paths.root}/${config.paths.js}/${prefix}${filename}`;
+            return `${config.paths.root}/${config.paths[task]}/${prefix}${filename}`;
+        });
     });
 });
